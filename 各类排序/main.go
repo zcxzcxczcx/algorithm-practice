98ba524290cbf550c660374a21a349335da6127c
zcxzcxczcx <1637960435@qq.com>
2021-06-28 08:20:10 +0800

    allsort

--- 各类排序/main.go
+++ 各类排序/main.go
@@ -7,7 +7,8 @@ import (
 
 func main() {
 	arr := []int{3, 1, 5, 2, 6, 9, 2, 5}
-	fmt.Printf("dddddddd=%v\n", MergeSort(arr, 0, 7))
+	buildBinaryTree(arr)
+	fmt.Printf("dddddddd=%v\n", countSorting(arr, 9))
 }
 
 // 选择排序
@@ -96,9 +97,7 @@ func MergeSort(arr []int, left, right int) []int {
 	}
 	mid := (left + right) / 2
 	arr = MergeSort(arr, left, mid)
-	fmt.Printf("dddddddddd111111111111=%v\n", arr)
 	arr = MergeSort(arr, mid+1, right)
-	fmt.Printf("dddddddddd2222222222=%v\n", arr)
 	return merge(arr, left, mid, right)
 }
 
@@ -137,4 +136,65 @@ func merge(arr []int, left, mid, right int) []int {
 
 	return arr
 
+}
+
+//堆排序
+// 下沉节点:删除的时候下沉
+func downAdjust(binaryTreeArr []int, parentIndex int) {
+	temp := binaryTreeArr[parentIndex]
+	childIndex := 2*parentIndex + 1
+	lenth := len(binaryTreeArr)
+	for childIndex < lenth {
+		if childIndex+1 < lenth && binaryTreeArr[childIndex+1] < binaryTreeArr[childIndex] {
+			childIndex = childIndex + 1
+		}
+		if temp < binaryTreeArr[childIndex] {
+			break
+		}
+		binaryTreeArr[parentIndex] = binaryTreeArr[childIndex]
+		parentIndex = childIndex
+		childIndex = 2*parentIndex + 1
+	}
+	binaryTreeArr[parentIndex] = temp
+}
+
+// 构建堆
+func buildBinaryTree(binaryTree []int) {
+	startIndex := (len(binaryTree) - 1) / 2
+	for i := startIndex; i >= 0; i-- {
+		downAdjust(binaryTree, i)
+	}
+}
+
+// 上浮节点,添加节点的时候上浮
+func upAdjust(binaryTreeArr []int) {
+	childIndex := len(binaryTreeArr) - 1
+	temp := binaryTreeArr[childIndex]
+	parentIndex := (childIndex - 1) / 2
+	for temp < binaryTreeArr[parentIndex] {
+		binaryTreeArr[childIndex] = binaryTreeArr[parentIndex]
+		childIndex = parentIndex
+		parentIndex = (childIndex - 1) / 2
+	}
+	binaryTreeArr[childIndex] = temp
+}
+
+// 计数排序
+func countSorting(arr []int, maxValue int) []int {
+	arrSort := []int{}
+	for i := 0; i < maxValue; i++ {
+		arrSort = append(arrSort, 0)
+	}
+	for i := 0; i < len(arr); i++ {
+		arrSort[arr[i]-1]++
+	}
+	arrSortReturn := []int{}
+	for i := 0; i < len(arrSort); i++ {
+		if arrSort[i] > 0 {
+			for j := 0; j < arrSort[i]; j++ {
+				arrSortReturn = append(arrSortReturn, i+1)
+			}
+		}
+	}
+	return arrSortReturn
 }
